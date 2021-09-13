5d4496b48d3861f318494e558a6de5023ee1a7a7
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-09-13 15:53:18 +0200

    Formatting config file

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import FormSignup from "./LoginForm";
-import Welcome from "./categories";
+import Welcome from "./categories";  
 
 const Form = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
